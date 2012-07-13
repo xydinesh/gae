42520eef097f154621cd66ed91e51bd3f79ce8c3
Dinesh Weerapurage <xydinesh@gmail.com>
2012-07-13 17:57:29 -0400

    adding gae helloworld

---
+++ tmp/helloworld/helloworld.py
@@ -0,0 +1,15 @@
+import webapp2
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp2.RequestHandler):
+        def get(self):
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.out.write("Hello, webapp World")
+            
+app = webapp2.WSGIApplication([('/', MainPage)])
+
+def main():
+    run_wsgi_app(app)
+
+if __name__ == '__main__':
+    main()  
